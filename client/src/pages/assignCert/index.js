c079faaa44605dad7adae4fec9621588489c4252
edwintamkc <edwintamkc@gmail.com>
2023-02-01 22:51:18 +0800

    init assign certificate

--- client/src/pages/assignCert/index.js
+++ client/src/pages/assignCert/index.js
@@ -1,5 +1,88 @@
-function AssignCert() {
-    return <div>assign cert</div>
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  Input,
+  Upload,
+  Space,
+  Select
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const { Option } = Select
+
+const AssignCert = () => {
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>Assign certificate</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: '请输入文章标题' }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: '请选择文章频道' }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              <Option value={0}>推荐</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Upload
+              name="image"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList
+            >
+              <div style={{ marginTop: 8 }}>
+                <PlusOutlined />
+              </div>
+            </Upload>
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: '请输入文章内容' }]}
+          ></Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布文章
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default AssignCert
