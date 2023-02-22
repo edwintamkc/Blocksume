cfb462b000b2220101f3c593f75d90d9ca727380
edwintamkc <edwintamkc@gmail.com>
2023-02-22 17:12:42 +0800

    update db config

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ import mysql from 'mysql'
 const db = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'admin123',
+    password: '123456',
     database: 'my_db_01'
 })
 
